0c0601c302f5604a6bf1e6597e60e2532fb15c11
Jeff Kehres <jeff.kehres@gmail.com>
2017-07-19 15:28:33 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,23 @@
+# serverless-plugin-info-json
+
+A [Serverless](https://serverless.com/) plugin that adds JSON and file output options to the [`info`](https://serverless.com/framework/docs/providers/aws/cli-reference/info/) command.
+
+## Installation
+
+First, install the plugin via npm:
+
+`npm install serverless-plugin-info-json`
+
+Second, edit your `serverless.yml` file and add the following section:
+
+```yaml
+plugins:
+  - serverless-plugin-info-json
+```
+
+## Usage
+
+In addition to the normal command line arguments provided by the `info` command, this plugin allows you use the following extra arguments as well:
+
+* `--json` / `-j` Format output as JSON instead of text.
+* `--file <FILE>` / `-f <FILE>` Send command output to the specified file instead of stdout.
---
+++ index.js
@@ -0,0 +1,101 @@
+'use strict';
+
+const fs = require('fs');
+const _ = require('lodash');
+
+class InfoJson {
+	constructor(serverless, options) {
+		this.serverless = serverless;
+		this.options = options;
+		this.provider = this.serverless.getProvider('aws');
+
+		this.commands = {
+			info: {
+				options: {
+					json: {
+						usage: 'Output as JSON',
+						shortcut: 'j',
+					},
+					file: {
+						usage: 'Output to a file',
+						shortcut: 'f'
+					}
+				}
+			}
+		};
+
+		this.hooks = {
+			'before:info:info': this.patchInfoPlugin.bind(this)
+		};
+	}
+
+	getInfoPlugin() {
+		return this.serverless.pluginManager.getPlugins().find((element) => element.constructor.name === 'AwsInfo');
+	}
+
+	patchInfoPlugin() {
+		if (this.options.file) {
+			const s = fs.createWriteStream(this.options.file);
+			process.stdout.write = process.stderr.write = s.write.bind(s);
+		}
+		if (this.options.json) {
+			const infoPlugin = this.getInfoPlugin();
+			infoPlugin.display = this.displayJson.bind(this);
+		}
+	}
+
+	displayJson() {
+		const gatheredData = this.getInfoPlugin().gatheredData;
+		let data = {
+			info: {
+				service: gatheredData.info.service,
+				stage: gatheredData.info.stage,
+				region: gatheredData.info.region,
+				apiKeys: {},
+				endpoints: [],
+				functions: {}
+			}
+		};
+
+		gatheredData.info.apiKeys.forEach((key) => {
+			data.info.apiKeys[key.name] = key.value;
+		});
+
+		if (gatheredData.info.endpoint) {
+			_.forEach(this.serverless.service.functions, (functionObject) => {
+				functionObject.events.forEach(event => {
+					if (event.http) {
+						let method;
+						let path;
+
+						if (typeof event.http === 'object') {
+							method = event.http.method.toUpperCase();
+							path = event.http.path;
+						} else {
+							method = event.http.split(' ')[0].toUpperCase();
+							path = event.http.split(' ')[1];
+						}
+						path = path !== '/' ? `/${path.split('/').filter(p => p !== '').join('/')}` : '';
+
+						data.info.endpoints.push({method, path});
+					}
+				});
+			});
+		}
+
+		gatheredData.info.functions.forEach((func) => {
+			data.info.functions[func.name] = func.deployedName;
+		});
+
+		if (this.options.verbose) {
+			data.outputs = {};
+			gatheredData.outputs.forEach((output) => {
+				data.outputs[output.OutputKey] = output.OutputValue;
+			});
+		}
+
+		this.serverless.cli.consoleLog(JSON.stringify(data, null, 2));
+	}
+}
+
+module.exports = InfoJson;
